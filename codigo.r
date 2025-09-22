9afc5a0bc77ec024700205f5241c0b03ae077ac0
zaakcs <claudioleiite@gmail.com>
2025-09-22 11:59:47 -0300

    Primeiro commit – projeto R

---
+++ codigo.r
@@ -0,0 +1,12 @@
+print("Olá, Mundo!")
+
+soma <- 5 + 3
+print(soma)
+
+calcular_area_retangulo <- function(largura, altura) {
+  area <- largura * altura
+  return(area)
+}
+
+area_do_meu_retangulo <- calcular_area_retangulo(10, 5)
+print(area_do_meu_retangulo)
